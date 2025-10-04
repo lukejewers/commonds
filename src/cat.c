e40fae80555d850cef3bdff4ad3f7740032abe7d
Luke <luke.jewers@gmail.com>
2025-10-04 21:14:37 +0100

    cat options
    * -b
    * -n

--- src/cat.c
+++ src/cat.c
@@ -1,56 +1,70 @@
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #define BUFF_SIZE 1024
 
-char *shift(char ***xs, int *xs_sz) {
-    assert((*xs_sz) > 0);
-    char *arg = **xs;
-    (*xs)++;
-    (*xs_sz)--;
-    return arg;
-}
+typedef enum {
+    OPTION_NONE, // No option.
+    OPTION_b,    // Number the non-blank output lines, starting at 1.
+    OPTION_n,    // Number the output lines, starting at 1.
+} Option;
 
-void cat_file(FILE *file) {
-    char buffer[BUFF_SIZE];
-    size_t bytes_read;
-    while ((bytes_read = fread(buffer, 1, sizeof(buffer), file)) > 0) {
-        fwrite(buffer, 1, bytes_read, stdout);
-    }
-}
-
-void cat_interactive(FILE *file) {
+void cat_file(FILE *file, Option option) {
     char line[BUFF_SIZE];
-    while (fgets(line, sizeof(line), file) != NULL) {
-        fputs(line, stdout);
-        fflush(stdout);
+    switch (option) {
+    case OPTION_b: {
+        int line_count = 1;
+        while (fgets(line, sizeof(line), file) != NULL) {
+            if (line[0] == '\n') printf("\n");
+            else printf("%6d\t%s", line_count++, line);
+        }
+        break;
+    }
+    case OPTION_n: {
+        int line_count = 1;
+        while (fgets(line, sizeof(line), file) != NULL) {
+            printf("%6d\t%s", line_count++, line);
+        }
+        break;
+    }
+    default:
+        while (fgets(line, sizeof(line), file) != NULL) {
+            printf("%s", line);
+        }
     }
 }
 
 int main(int argc, char **argv) {
-    int exit_status = 0;
-    shift(&argv, &argc); // program name
+    Option option = OPTION_NONE;
+    bool has_stdin_arg = false;
 
-    if (argc == 0) {
-        if (isatty(fileno(stdin))) {
-            fflush(stdout);
-            cat_interactive(stdin);
-        } else {
-            cat_file(stdin);
+    if (argc > 1) {
+        if (strcmp(argv[1], "-b") == 0) option = OPTION_b;
+        else if (strcmp(argv[1], "-n") == 0) option = OPTION_n;
+        else if (strcmp(argv[1], "-") == 0 || strcmp(argv[1], "--") == 0) has_stdin_arg = true;
+        if (option != OPTION_NONE) {
+            argc--;
+            argv++;
         }
+    }
+
+    if (argc == 1 || has_stdin_arg) {
+        cat_file(stdin, option);
         return 0;
     }
 
-    while (argc > 0) {
-        char *next_arg = shift(&argv, &argc);
-        FILE *next_fptr = fopen(next_arg, "r");
-        if (next_fptr == NULL) {
-            fprintf(stderr, "cat: %s: No such file or directory\n", next_arg);
+    int exit_status = 0;
+    for (int i = 1; i < argc; i++) {
+        FILE *file = fopen(argv[i], "r");
+        if (file == NULL) {
+            fprintf(stderr, "cat: %s: No such file or directory\n", argv[i]);
             exit_status = 1;
         } else {
-            cat_file(next_fptr);
-            fclose(next_fptr);
+            cat_file(file, option);
+            fclose(file);
         }
     }
 
